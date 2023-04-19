147c4f1426a8e160413a400c5e9b5a1638f2e88e
santoshdwivedi90 <santosh.dwivedi88@gmail.com>
2023-04-19 16:00:48 +0200

    update postman collections and shells script

--- triggertest.sh
+++ triggertest.sh
@@ -1,3 +1,2 @@
-echo "${bearertoken}"
 npm install -g newman
-newman run -d example-csv-all-types.csv  CreateUSers.postman_collection.json --global-var "bearertoken=${bearertoken}"
+newman run -d example-csv-all-types.csv  CreateUSers.postman_collection.json --global-var "bearertoken=${_B_TOKEN}"
