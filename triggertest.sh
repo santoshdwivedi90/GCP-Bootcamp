e9d4a7069d1665d54f19c2b57be7d33242acfbf0
santoshdwivedi90 <santosh.dwivedi88@gmail.com>
2023-04-19 15:28:22 +0200

    update postman collections and shells script

--- triggertest.sh
+++ triggertest.sh
@@ -1,2 +1,3 @@
+bearertoken=$(gcloud auth print-identity-token)
 npm install -g newman
-newman run -d example-csv-all-types.csv  CreateUSers.postman_collection.json
+newman run -d example-csv-all-types.csv  CreateUSers.postman_collection.json --global-var "bearertoken=${bearertoken}"
