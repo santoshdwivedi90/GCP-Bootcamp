353473cfeb5beefda2eb77d6c1579e32d81adb4a
santoshdwivedi90 <santosh.dwivedi88@gmail.com>
2023-04-20 11:48:33 +0200

    generate identity token

--- triggertest.sh
+++ triggertest.sh
@@ -1,3 +1,3 @@
-echo ${bearertoken}
+
 npm install -g newman
-newman run -d example-csv-all-types.csv  CreateUSers.postman_collection.json --global-var "bearertoken=${bearertoken}"
+newman run -d example-csv-all-types.csv  CreateUSers.postman_collection.json
